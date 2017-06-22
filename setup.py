a3f6a5b547c1d16d5a16a2e36885dbd460a435b9
Aleksandr Kuzminsky <aleksandr.kuzminsky@gmail.com>
2017-06-22 13:24:04 -0700

    Bump version: 1.5.0 → 1.5.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [str(ir.req) for ir in
 
 setup(
     name='etcdb',
-    version='1.5.0',
+    version='1.5.1',
     description="PEP 249 compatible driver for Etcd",
     long_description=readme + '\n\n' + history,
     author="Box TechOps Database Team",
