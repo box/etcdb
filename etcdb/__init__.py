bc8371edef93951eb74342bac74ccbf9065584a5
Aleksandr Kuzminsky <aleksandr.kuzminsky@gmail.com>
2017-11-09 10:50:46 -0800

    Bump version: 1.5.11 → 1.5.12

--- etcdb/__init__.py
+++ etcdb/__init__.py
@@ -22,7 +22,7 @@ from .etcdtimestamp import EtcdTimestamp
 
 __author__ = 'Box TechOps Database Team'
 __email__ = 'oss@box.com'
-__version__ = '1.5.11'
+__version__ = '1.5.12'
 
 
 def _split_version(version):
